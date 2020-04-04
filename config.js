45c2c9175872872cb5304c9aac5e9f6c3f8e13bf
boomgreen87 <boomgreen87@yahoo.com>
2020-04-03 23:47:10 -0400

    Quick change

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const config = {
-    // host: "us-cdbr-iron-east-05.cleardb.net",
-    // // port: 3306, // 8889 for mac
-    // uname: "ba293c82b79954",
-    // pword: "054bea07", 
-    // database: "heroku_f9c4e3f7f05a77b"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    // port: 3306, // 8889 for mac
+    uname: "ba293c82b79954",
+    pword: "054bea07", 
+    database: "heroku_f9c4e3f7f05a77b"
 
-    host: "localhost",
-    port: 3306, // 8889 for mac
-    uname: "root",
-    pword: "", 
-    database: "db_portfolio"
+    // host: "localhost",
+    // port: 3306, // 8889 for mac
+    // uname: "root",
+    // pword: "", 
+    // database: "db_portfolio"
 }
 
 module.exports = config;
